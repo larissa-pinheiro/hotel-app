9c61b3e3dd3178ca1ce40e83f51a1ecc30612589
Larissa Pinheiro <rissapins@gmail.com>
2024-12-27 17:05:20 -0300

    create the project structure

---
+++ src/app/models/reservation.ts
@@ -0,0 +1,9 @@
+export interface Reservation {
+  id: string,
+  checkInDate: Date,
+  checkOutDate: Date,
+  guestName: string,
+  guestEmail: string,
+  guestPhone: number,
+  roomNumber: number
+}
